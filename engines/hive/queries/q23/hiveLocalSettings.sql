4b3528f34b1a6f22acffbfa72e620ced63884240
XiaominZhang <xiaomin.zhang@intel.com>
2015-08-24 11:15:55 +0800

    use lower cased value

--- engines/hive/queries/q23/hiveLocalSettings.sql
+++ engines/hive/queries/q23/hiveLocalSettings.sql
@@ -1,9 +1,9 @@
 set mapred.max.split.size=67108864;
-set hive.auto.convert.join=True;
-set hive.auto.convert.join.noconditionaltask=True;
+set hive.auto.convert.join=true;
+set hive.auto.convert.join.noconditionaltask=true;
 set hive.auto.convert.join.noconditionaltask.size=100000000;
 set hive.groupby.skewindata=false;
-set hive.exec.parallel=True;
+set hive.exec.parallel=true;
 set hive.exec.parallel.thread.number=8;
 
 
